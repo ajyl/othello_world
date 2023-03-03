7f5e603116a54656649bf1ee29914dff44975815
Andrew Lee <ajyl@umich.edu>
2023-03-03 01:50:46 -0500

    extracting residual streams..

--- train_probe_lens.py
+++ train_probe_lens.py
@@ -35,6 +35,8 @@ from mingpt.probe_model import (
     BatteryProbeClassification,
     BatteryProbeClassificationTwoLayer,
 )
+from lens.testing import load_lens_model
+
 
 parser = argparse.ArgumentParser(description="Train classification network")
 
@@ -58,9 +60,6 @@ print(f"Running experiment for {folder_name}")
 othello = get_othello(ood_num=-1)
 
 train_dataset = CharDataset(othello)
-if torch.cuda.is_available():
-    device = torch.cuda.current_device()
-    model = model.to(device)
 
 
 PROP_BATCH_SIZE=8192
@@ -91,8 +90,8 @@ def get_age(x):
  
 
 act_container = []
-num_proc = multiprocessing.cpu_count()
-p = multiprocessing.Pool(num_proc)
+#num_proc = multiprocessing.cpu_count()
+#p = multiprocessing.Pool(num_proc)
 
 #for idx, (x, y, z) in tqdm(enumerate(loader), total=len(loader)):
 #    output_filepath = os.path.join(
@@ -123,6 +122,65 @@ p = multiprocessing.Pool(num_proc)
 #
 #    with open(output_filepath, "wb") as file_p:
 #        pickle.dump(age_container, file_p)
+
+
+model = load_lens_model()
+if torch.cuda.is_available():
+    device = torch.cuda.current_device()
+    model = model.to(device)
+
+
+ACT_BATCH_SIZE=512
+loader = DataLoader(
+    train_dataset, shuffle=False, pin_memory=True, batch_size=ACT_BATCH_SIZE
+)
+CACHE_LAYER = "blocks.6.hook_resid_post"
+acts = []
+fastforward = True
+
+for idx, (x, y, z) in tqdm(enumerate(loader), total=len(loader)):
+
+
+    if idx % 100 == 0:
+        lookahead_idx = idx + 100
+        lookahead_path = os.path.join(
+            "/home/ajyl/othello_world",
+            f"probe_data/resid_6_post_{lookahead_idx}.pkl"
+        )
+        if not os.path.isfile(lookahead_path):
+            fastfoward = False
+            print("Will start collecting activations.")
+
+        else:
+            print(f"Fast-forwarding until {lookahead_idx}.")
+
+    if fastfoward:
+        continue
+
+    if idx % 100 == 0:
+        output_filepath = os.path.join(
+            "/home/ajyl/othello_world",
+            f"probe_data/resid_6_post_{idx}.pkl"
+        )
+
+        look_ahead = idx + 100
+        lookahead_path = os.path.join(
+            "/home/ajyl/othello_world",
+            f"probe_data/resid_6_post_{idx}.pkl"
+        )
+        #output_filepath = os.path.join(
+        #    "/scratch/mihalcea_root/mihalcea98/ajyl",
+        #    f"probe_data/resid_6_post_{idx}.pkl"
+        #)
+        print("Mem usage of acts:", sys.getsizeof(acts))
+        torch.save(acts, output_filepath)
+        acts = []
+
+    logits, cache = model.run_with_cache(x)
+    act = cache[CACHE_LAYER] # [Batch (ACT_BATCH_SIZE), Seq len (59), h_dim (512)]
+    act = act.reshape(-1, act.shape[-1])
+    acts.extend(act)
+
 
 
 
